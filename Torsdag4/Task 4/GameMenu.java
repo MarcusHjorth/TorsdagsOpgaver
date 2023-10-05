f6313a15e2479165ef47d5666e850dc6de6c25d9
Marcus Rasmussen <marcusphr1808@gmail.com>
2023-10-05 16:42:39 +0200

    Torsdags4 task 4 update?

--- Torsdag4/Task 4/GameMenu.java
+++ Torsdag4/Task 4/GameMenu.java
@@ -1,10 +1,28 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GameMenu{
 
 	ArrayList<String> actions = new ArrayList();
 
 	public GameMenu(ArrayList<String> actions) {
-		ArrayList<String> actions = actions;
+		this.actions = actions;
+	}
+
+	public void displayMenu(){
+		System.out.println("GameMenu: ");
+		for ( int i = 0 ; i < actions.size(); i++ ){
+			System.out.println(( i ) + ". " + actions.get(i));
+		}
+	}
+
+	public String getAction(){
+		System.out.println("Type a number to choose an action");
+		displayMenu();
+
+		Scanner scanner = new Scanner(System.in);
+		String choice = scanner.nextLine();
+
+		return choice; 
 	}
 }
--- Torsdag4/Task 4/Main.java
+++ Torsdag4/Task 4/Main.java
@@ -1,15 +1,43 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 	public static void main (String[] args){
 		
-		ArrayList<String> actions = new ArrayList();
-		actions.add("Start Game");
-		actions.add("Resume Game");
-		actions.add("Pause Game");
-		actions.add("End Game");
+		ArrayList<String> menuItems = new ArrayList<String>();
+		menuItems.add("Start Game");
+		menuItems.add("Resume Game");
+		menuItems.add("Pause Game");
+		menuItems.add("End Game");
 
-		System.out.println(actions);
-		System.out.print(actions.get(2));
+		//System.out.println(menuItems);
+		//System.out.print(menuItems.get(2));
+
+		GameMenu gameMenu = new GameMenu(menuItems);
+		gameMenu.displayMenu();
+	
+
+		String userChoice = gameMenu.getAction();
+
+		int choice = Integer.parseInt(userChoice);
+		doAction(choice);
+
+	}
+
+	public static void doAction(int action){
+		switch(action){
+			case 1: 
+				System.out.println("Starting game now");
+				break;
+			case 2: 
+				System.out.println("Resume");
+				break;
+			case 3: 
+				System.out.println("Pausing game");
+				break;
+			case 4: 
+				System.out.println("End game");
+				break;
+		}
 	}
 }
